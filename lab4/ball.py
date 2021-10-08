8f1f84d230483312370c66805f118297c23fbece
Parfenov Valery <parfenov.vr@phystech.edu>
2021-10-08 10:29:59 +0300

    добавлена документация

--- lab4/ball.py
+++ lab4/ball.py
@@ -4,9 +4,11 @@ from random import randint
 
 pygame.init()
 
-FPS = 2
-screen_width, screen_height = screen_size = (700, 700)
+# Задание констант
+FPS = 2 # частота обновления экрана
+screen_width, screen_height = screen_size = (700, 700) # параметры размера экранов
 screen = pygame.display.set_mode(screen_size)
+BALL_RADIUS_RANGE = [10, 50] # минимальный и максимальный размеры шарика
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -16,12 +18,13 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-BALL_RADIUS = [10, 50]
-COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, BLACK]
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, BLACK] #список всех возможных окрасок шариков
 
-def new_ball(BALL_RADIUS = []):
-    '''рисует новый шарик '''
-    r = randint(BALL_RADIUS[0], BALL_RADIUS[1])
+def new_ball(BALL_RADIUS_RANGE = []):
+    '''рисует новый шарик
+    :param BALL_RADIUS_RANGE = []: [mib_ball_radius, max_ball_radius]
+    '''
+    r = randint(BALL_RADIUS_RANGE[0], BALL_RADIUS_RANGE[1])
     x = randint(r, screen_width - r)
     y = randint(r, screen_height - r)
     color = COLORS[randint(0, len(COLORS) - 1)]
@@ -31,7 +34,7 @@ def new_ball(BALL_RADIUS = []):
 screen.fill(WHITE)
 pygame.display.update()
 clock = pygame.time.Clock()
-finished = False
+finished = False # флажок, показывающий, не произошёл ли QUIT
 
 while not finished:
     clock.tick(FPS)
@@ -40,7 +43,7 @@ while not finished:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print('Click!')
-    new_ball(BALL_RADIUS)
+    new_ball(BALL_RADIUS_RANGE)
     pygame.display.update()
     screen.fill(WHITE)
 
