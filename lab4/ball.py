4a9ac2d0f8981211cedbfcca14ba5af7767c1e1e
Parfenov Valery <parfenov.vr@phystech.edu>
2021-10-15 10:44:46 +0300

    шаг модели и обработка события вынесена в model.py
    модель пока не поноценная (размеры и время не самостоятельны)
    
    интерфейс пока кривой, документация тоже

--- lab4/ball.py
+++ lab4/ball.py
@@ -18,53 +18,6 @@ FONT1 = pygame.font.Font(None, 40)  # шрифт для счётчика
 WHITE = (255, 255, 255)
 
 # задаём пеоеменные
-counter = 0  # счётчик очков
-
-
-def click_check_slot(balls, BALLS_AMOUNT, mouse_button, mouse_coords=()):
-    '''
-    функция проверяет, попал ли пользователь в шарик. Если попал - возвращается номер шарика,
-    в который попал пльзователь, иначе - -1
-    :param balls: - двумерный массив с параметрами шариков
-    :param BALLS_AMOUNT: - количество шариков
-    :param mouse_coords: - координаты нажатия
-    :param mouse_button: - номер нажатой кнопки
-    '''
-    for i in range(0, BALLS_AMOUNT):
-        distance = int(((balls[i][0] - mouse_coords[0]) ** 2 + (balls[i][1] - mouse_coords[1]) ** 2) ** 0.5)
-        if (balls[i][4] >= distance):  # balls[i][4] - радиус i шарика
-            return i
-    return -1
-
-
-def ball_motion(balls, BALLS_AMOUNT, screensize=[]):
-    '''
-    функция отвечает за движение шарика, аргумент balls - двумерный массив с началньным
-    состоянием шариков, возвращает конечное
-    '''
-    for i in range(0, BALLS_AMOUNT):  # для каждого шарика осуществим эволюцию параметров
-        x = balls[i][0]  # переобозначим параметры шарика
-        y = balls[i][1]
-        v_x = balls[i][2]
-        v_y = balls[i][3]
-        r = balls[i][4]
-        if (x < r - v_x):  # здесь реализуется случайное отражение от стен
-            v_x = randint(BALL_VELOCITY_RANGE[0], BALL_VELOCITY_RANGE[1])
-        elif (screensize[0] - x - v_x < r):
-            v_x = -1 * randint(BALL_VELOCITY_RANGE[0], BALL_VELOCITY_RANGE[1])
-        if (y < r - v_y):
-            v_y = randint(BALL_VELOCITY_RANGE[0], BALL_VELOCITY_RANGE[1])
-        elif (screensize[1] - y - v_y < r):
-            v_y = -1 * randint(BALL_VELOCITY_RANGE[0], BALL_VELOCITY_RANGE[1])
-        x += v_x  # осуществляется перемещение шарика
-        y += v_y
-        balls[i][0] = x
-        balls[i][1] = y
-        balls[i][2] = v_x
-        balls[i][3] = v_y
-        balls[i][4] = r
-    return balls
-
 
 def aim_drower(balls):
     '''
@@ -87,13 +40,10 @@ while not finished:  # главный цикл
         if event.type == pygame.QUIT:  # проверка на QUIT
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:  # обработка нажатия мыши
-            ball_id = click_check_slot(balls, BALLS_AMOUNT, event.button, event.pos)
-            if (ball_id != -1):
-                counter += 1
-                balls[ball_id] = ball_create(BALL_RADIUS_RANGE, SCREEN_SIZE, BALL_VELOCITY_RANGE)
-    balls = ball_motion(balls, BALLS_AMOUNT, SCREEN_SIZE)
+            model.click_check_slot(event.button, event.pos)
+    balls = model.ball_motion()
     aim_drower(balls)
-    text1 = FONT1.render(str(counter), False, (0, 0, 0))  # задаём счётчик
+    text1 = FONT1.render(str(model.counter), False, (0, 0, 0))  # задаём счётчик
     screen.blit(text1, (10, 10))  # отображаем счётчик
     pygame.display.update()
     screen.fill(WHITE)
--- lab4/model.py
+++ lab4/model.py
@@ -1,6 +1,6 @@
 from random import randint
 
-global BALL_RADIUS_RANGE, BALL_VELOCITY_RANGE, BALLS_AMOUNT, COLORS, SCREEN_SIZE
+global BALL_RADIUS_RANGE, BALL_VELOCITY_RANGE, BALLS_AMOUNT, COLORS, SCREEN_SIZE, counter
 BALL_RADIUS_RANGE = [30, 50]  # минимальный и максимальный размеры шарика
 BALL_VELOCITY_RANGE = [3, 20]  # минимальная и максимальная скорость шарика
 BALLS_AMOUNT = 6  # количество шариков
@@ -14,6 +14,8 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, BLACK]  # список всех возможных окрасок шариков
+
+counter = 0
 
 def ball_create(BALL_RADIUS_RANGE=[], SCREEN_SIZE=(), BALL_VELOCITY_RANGE=[]):
     '''
@@ -37,3 +39,51 @@ def model_init():
     balls = [[] * 7] * BALLS_AMOUNT  # создаём начальную систему шариков
     for i in range(0, BALLS_AMOUNT):
         balls[i] = ball_create(BALL_RADIUS_RANGE, SCREEN_SIZE, BALL_VELOCITY_RANGE)
+
+def ball_motion():
+    '''
+    функция отвечает за движение шарика, аргумент balls - двумерный массив с началньным
+    состоянием шариков, возвращает конечное
+    '''
+    for i in range(0, BALLS_AMOUNT):  # для каждого шарика осуществим эволюцию параметров
+        x = balls[i][0]  # переобозначим параметры шарика
+        y = balls[i][1]
+        v_x = balls[i][2]
+        v_y = balls[i][3]
+        r = balls[i][4]
+        if (x < r - v_x):  # здесь реализуется случайное отражение от стен
+            v_x = randint(BALL_VELOCITY_RANGE[0], BALL_VELOCITY_RANGE[1])
+        elif (SCREEN_SIZE[0] - x - v_x < r):
+            v_x = -1 * randint(BALL_VELOCITY_RANGE[0], BALL_VELOCITY_RANGE[1])
+        if (y < r - v_y):
+            v_y = randint(BALL_VELOCITY_RANGE[0], BALL_VELOCITY_RANGE[1])
+        elif (SCREEN_SIZE[1] - y - v_y < r):
+            v_y = -1 * randint(BALL_VELOCITY_RANGE[0], BALL_VELOCITY_RANGE[1])
+        x += v_x  # осуществляется перемещение шарика
+        y += v_y
+        balls[i][0] = x
+        balls[i][1] = y
+        balls[i][2] = v_x
+        balls[i][3] = v_y
+        balls[i][4] = r
+    return balls
+
+def click_check_slot(mouse_button, mouse_coords=()):
+    '''
+    функция проверяет, попал ли пользователь в шарик. Если попал - возвращается номер шарика,
+    в который попал пльзователь, иначе - -1
+    :param balls: - двумерный массив с параметрами шариков
+    :param BALLS_AMOUNT: - количество шариков
+    :param mouse_coords: - координаты нажатия
+    :param mouse_button: - номер нажатой кнопки
+    '''
+    global counter
+    aimed_ball = -1
+    for i in range(0, BALLS_AMOUNT):
+        distance = int(((balls[i][0] - mouse_coords[0]) ** 2 + (balls[i][1] - mouse_coords[1]) ** 2) ** 0.5)
+        if (balls[i][4] >= distance):  # balls[i][4] - радиус i шарика
+            aimed_ball = i
+            break
+    if (aimed_ball != -1):
+        counter += 1
+        balls[aimed_ball] = ball_create(BALL_RADIUS_RANGE, SCREEN_SIZE, BALL_VELOCITY_RANGE)
